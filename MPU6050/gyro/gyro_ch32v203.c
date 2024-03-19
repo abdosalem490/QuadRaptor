47645aa7441a22a91ef70f0680275b5906c25c2b
Mohabz-911 <hoobamz@gmail.com>
2024-03-19 22:51:13 +0200

    Gyro on ch32v203 [wrapper]

--- MPU6050/gyro/gyro_ch32v203.c
+++ MPU6050/gyro/gyro_ch32v203.c
@@ -12,11 +12,14 @@
 
 #include "debug.h"
 
+/* I2C master mode flag */
+#define MASTER_RECEIVER_MODE 1
+
 /* Receiver buffer size */
-#define Size   6
+#define I2C_Rx_BUFFER_SIZE   6
 
 /* Receiver buffer */
-u8 RxData[Size];
+u8 RxData[I2C_Rx_BUFFER_SIZE];
 
 /* Flight angle rates to be computed by gyroscope measurements */
 float RateRoll, RatePitch, RateYaw;
@@ -24,95 +27,69 @@ float RateRoll, RatePitch, RateYaw;
 /* Just a character buffer to print the angle rates */
 char roll_buffer[10], pitch_buffer[10], yaw_buffer[10];
 
-void gyro_signals(void) {
-    int16_t GyroX, GyroY, GyroZ, i = 0;
-    while( I2C_GetFlagStatus( I2C2, I2C_FLAG_BUSY ) != RESET );
+void I2C_init() __attribute__((always_inline));
+void I2C_start_transmission(uint8_t receiver_address, uint8_t master_receive) __attribute__((always_inline));
+void I2C_write(uint8_t value) __attribute__((always_inline));
+void I2C_stop() __attribute__((always_inline));
+uint8_t I2C_requestFrom(uint8_t address, uint8_t quantity, uint8_t stop_bool) __attribute__((always_inline));
+uint8_t I2C_read() __attribute__((always_inline));
 
-    I2C_GenerateSTART(I2C2, ENABLE);
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_MODE_SELECT ) );
+inline void I2C_start_transmission(uint8_t receiver_address, uint8_t master_receive = 0){
+    uint8_t i2c_dir = I2C_Direction_Transmitter;
+    uint32_t i2c_event = I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED;
 
-    I2C_Send7bitAddress(I2C2, 0x68 << 1, I2C_Direction_Transmitter);
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
-
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_SendData(I2C2, 0x1A);
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_SendData(I2C2, 0x05);
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_GenerateSTOP(I2C2, ENABLE);
-
-    ////////////////////////////////////////////////////
+    if(master_receive){
+        i2c_dir = I2C_Direction_Receiver;
+        i2c_event = I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED;
+    }
 
     while( I2C_GetFlagStatus( I2C2, I2C_FLAG_BUSY ) != RESET );
 
     I2C_GenerateSTART(I2C2, ENABLE);
     while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_MODE_SELECT ) );
 
-    I2C_Send7bitAddress(I2C2, 0x68 << 1, I2C_Direction_Transmitter);
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
+    I2C_Send7bitAddress(I2C2, receiver_address << 1, i2c_dir);
+    while( !I2C_CheckEvent( I2C2, i2c_event ) );
+}
 
+inline void I2C_write(uint8_t value){
     while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_SendData(I2C2, 0x1B);
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_SendData(I2C2, 0x08);
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_GenerateSTOP(I2C2, ENABLE);
+    I2C_SendData(I2C2, value);
+}
 
-    ///////////////////////////////////////////////////
-
-    while( I2C_GetFlagStatus( I2C2, I2C_FLAG_BUSY ) != RESET );
-
-    I2C_GenerateSTART(I2C2, ENABLE);
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_MODE_SELECT ) );
-
-    I2C_Send7bitAddress(I2C2, 0x68 << 1, I2C_Direction_Transmitter);
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
-
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_SendData(I2C2, 0x43);
-
+inline void I2C_stop(){
     while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
     I2C_GenerateSTOP(I2C2, ENABLE);
+}
 
-    ////////////////////////////////////////////////////
+inline uint8_t I2C_read(){
+    static uint8_t i = 0;
+    uint8_t result = RxData[i];
+    i = (i+1) % I2C_Rx_BUFFER_SIZE;
+    return result;
+}
 
-    while( I2C_GetFlagStatus( I2C2, I2C_FLAG_BUSY ) != RESET );
+inline uint8_t I2C_requestFrom(uint8_t address, uint8_t quantity, uint8_t stop_bool = true){
+    uint8_t i = 0;
 
-    I2C_GenerateSTART( I2C2, ENABLE );
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_MODE_SELECT ) );
+    I2C_start_transmission(address, MASTER_RECEIVER_MODE);
 
-    I2C_Send7bitAddress( I2C2, 0x68<<1, I2C_Direction_Receiver );
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED  ) );
-
-
-    while(i < 6){
+    while(i < I2C_Rx_BUFFER_SIZE){
         if( I2C_GetFlagStatus( I2C2, I2C_FLAG_RXNE ) !=  RESET ){
             RxData[i] = I2C_ReceiveData( I2C2 );
             i++;
         }
-        if(i == 5) I2C_NACKPositionConfig(I2C2, I2C_NACKPosition_Next);
+        if(i == I2C_Rx_BUFFER_SIZE-1) I2C_NACKPositionConfig(I2C2, I2C_NACKPosition_Next);
     }
+
     while(I2C_GetFlagStatus(I2C2, I2C_FLAG_RXNE)!=SET);
     I2C_GenerateSTOP(I2C2, ENABLE);
-
-    GyroX= RxData[0]<<8 | RxData[1];
-    GyroY= RxData[2]<<8 | RxData[3];
-    GyroZ= RxData[4]<<8 | RxData[5];
-    RateRoll=(float)GyroX/65.5;
-    RatePitch=(float)GyroY/65.5;
-    RateYaw=(float)GyroZ/65.5;
-
-    gcvt(RateRoll, 6, roll_buffer);
-    gcvt(RatePitch, 6, pitch_buffer);
-    gcvt(RateYaw, 6, yaw_buffer);
-
+    
+    return i;
 }
 
-int main(void)
+void I2C_init_ch32()
 {
-    USART_Printf_Init(57600);
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-    Delay_Init();
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
 
@@ -132,22 +109,58 @@ int main(void)
     I2CConfig.I2C_Ack = I2C_Ack_Enable;
     I2CConfig.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
     I2C_Init(I2C2, &I2CConfig);
+}
+
+inline void I2C_init(){
+    I2C_init_ch32();
+}
 
 
-    while( I2C_GetFlagStatus( I2C2, I2C_FLAG_BUSY ) != RESET );
+void gyro_signals(void) {
+    int16_t GyroX, GyroY, GyroZ;
 
-    I2C_GenerateSTART(I2C2, ENABLE);
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_MODE_SELECT ) );
+    I2C_start_transmission(0x68);
+    I2C_write(0x1A);
+    I2C_write(0x05);
+    I2C_stop();
+    ////////////////////////////////////////////////////
+    I2C_start_transmission(0x68);
+    I2C_write(0x1B);
+    I2C_write(0x08);
+    I2C_stop();
+    ///////////////////////////////////////////////////
+    I2C_start_transmission(0x68);
+    I2C_write(0x43);
+    I2C_stop();
+    ////////////////////////////////////////////////////
+    I2C_requestFrom(0x68, I2C_Rx_BUFFER_SIZE);
 
-    I2C_Send7bitAddress(I2C2, 0x68 << 1, I2C_Direction_Transmitter);
-    while( !I2C_CheckEvent( I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
+    GyroX= I2C_read()<<8 | I2C_read();
+    GyroY= I2C_read()<<8 | I2C_read();
+    GyroZ= I2C_read()<<8 | I2C_read();
 
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_SendData(I2C2, 0x6B);
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_SendData(I2C2, 0);
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE)!=SET);
-    I2C_GenerateSTOP(I2C2, ENABLE);
+    RateRoll=(float)GyroX/65.5;
+    RatePitch=(float)GyroY/65.5;
+    RateYaw=(float)GyroZ/65.5;
+
+    gcvt(RateRoll, 6, roll_buffer);
+    gcvt(RatePitch, 6, pitch_buffer);
+    gcvt(RateYaw, 6, yaw_buffer);
+
+}
+
+int main(void)
+{
+    USART_Printf_Init(57600);
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    Delay_Init();
+    I2C_init_ch32();
+
+
+    I2C_start_transmission(0x68);
+    I2C_write(0x6B);
+    I2C_write(0);
+    I2C_stop();
 
 
     while(1){
